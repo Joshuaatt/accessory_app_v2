f177e04f9c06e4ce96f530d4965e862939f2ccb9
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-03-31 12:47:15 -0700

    add seed data to populate manufacturers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+manufacturers = Manufacturer.find_or_create_by([{ name: 'Toyota'}, { name: 'Subaru' }])
