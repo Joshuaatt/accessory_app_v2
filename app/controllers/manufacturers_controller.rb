7d5a5992cee9472e414e2720b26619f63247da64
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-03-31 12:43:22 -0700

    add show, create, update, and destroy

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -4,6 +4,44 @@ class ManufacturersController < ApplicationController
   end
 
   def new
-    
+    @manufacturer = Manufacturer.new
+  end
+
+  def show
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+    if @manufacturer.save
+      flash[:notice] = "Manufacturer created successfully"
+      redirect_to manufacturers_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @manufacturer = Manufacturer.find(params[:id])
+  end
+
+  def update
+    @manufacturer = Manufacturer.find(params[:id])
+    if $manufacturer.update(manufacturer_params)
+      redirect_to manufacturers_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @manufacturer = Manufacturer.find(params[:id])
+    @manufacturer.destroy
+    redirect_to manufacturers_path
+  end
+
+private
+  def manufacturer_params
+    params.require(:manufacturer).permit(:name)
   end
 end
