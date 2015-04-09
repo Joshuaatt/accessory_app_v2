29d982eb2d4326661f2ef6a70194d6ef4d275966
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-04-08 21:32:38 -0700

    add models

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 Manufacturer.find_or_create_by id: 1, name: 'Toyota'
 Manufacturer.find_or_create_by id: 2, name: 'Subaru'
 
+#### TOYOTAS ####
+
 Model.find_or_create_by id: 1,
   name: "4Runner",
   year: "2015",
@@ -36,6 +38,102 @@ Model.find_or_create_by id: 4,
   image_file_name: "2015-toyota-corolla.jpg",
   image_content_type: "image/jpeg"
 
+Model.find_or_create_by id: 12,
+  name: "FR-S",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-scion-fr-s.jpeg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 13,
+  name: "Highlander",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-Toyota-Highlander.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 14,
+  name: "iQ",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-scion-iq.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 15,
+  name: "Land Cruiser",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-toyota-land-cruiser.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 16,
+  name: "Prius",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-toyota-prius.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 17,
+  name: "Prius C",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-toyota-prius-c.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 18,
+  name: "Prius V",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-Toyota-Prius-v.jpg",
+  image_content_type: "image/jpeg"
+
+
+Model.find_or_create_by id: 19,
+  name: "RAV4",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015_toyota_rav4.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 20,
+  name: "Sequoia",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-toyota-sequoia.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 21,
+  name: "Sienna",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-Toyota-Sienna.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 22,
+  name: "Tacoma",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015_toyota_tacoma.jpeg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 23,
+  name: "tC",
+  year: "2015",
+  manufacturer_id: 1,
+  image_file_name: "2015-scion-tc.jpg",
+  image_content_type: "image/jpeg"
+
+
+
+  # 2015 Tundra
+  # 2015 Venza
+  # 2015 xB
+  # 2015 xD
+  # 2015 Yaris
+  # 2014 Tundra
+
+#### SUBARUS ####
+
 Model.find_or_create_by id: 5,
   name: "Tribeca",
   year: "2014",
@@ -43,36 +141,52 @@ Model.find_or_create_by id: 5,
   image_file_name: "2014-subaru-tribeca.jpg",
   image_content_type: "image/jpeg"
 
-  # 2014 Tundra
-  # 2015 4Runner
-  # 2015 Avalon
-  # 2015 Camry
-  # 2015 Corolla
-  # 2015 FR-S
-  # 2015 Highlander
-  # 2015 iQ
-  # 2015 Land Cruiser
-  # 2015 Prius
-  # 2015 Prius C
-  # 2015 Prius V
-  # 2015 RAV4
-  # 2015 Sequoia
-  # 2015 Sienna
-  # 2015 Tacoma
-  # 2015 tC
-  # 2015 Tundra
-  # 2015 Venza
-  # 2015 xB
-  # 2015 xD
-  # 2015 Yaris
+Model.find_or_create_by id: 6,
+  name: "BRZ",
+  year: "2015",
+  manufacturer_id: 2,
+  image_file_name: "Subaru-2015-BRZ.jpg",
+  image_content_type: "image/jpeg"
 
-# 2014 Tribeca
-# 2015 BRZ
-# 2015 Forester
-# 2015 Impreza & Cross
-# 2015 Legacy
-# 2015 Outback
-# 2015 WRX & STI
+Model.find_or_create_by id: 7,
+  name: "Forester",
+  year: "2015",
+  manufacturer_id: 2,
+  image_file_name: "2015-forester.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 8,
+  name: "Impreza & Cross",
+  year: "2015",
+  manufacturer_id: 2,
+  image_file_name: "2015-impreza-and-cross.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 9,
+  name: "Legacy",
+  year: "2015",
+  manufacturer_id: 2,
+  image_file_name: "2015-legacy.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 10,
+  name: "Outback",
+  year: "2015",
+  manufacturer_id: 2,
+  image_file_name: "2015-subaru-outback.jpg",
+  image_content_type: "image/jpeg"
+
+Model.find_or_create_by id: 11,
+  name: "WRX & STI",
+  year: "2015",
+  manufacturer_id: 2,
+  image_file_name: "2015-subaru-wrx-sti.jpg",
+  image_content_type: "image/jpeg"
+
+
+
+
+
 
 Accessory.find_or_create_by id: 21,
   model_id: 1,
