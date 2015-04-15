850d7476ce7a05fdf6cb5b63c8a638b15102294c
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-04-15 11:50:16 -0700

    add some integration testing for user flow

--- spec/features/user_flow_through_spec.rb
+++ spec/features/user_flow_through_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
 describe 'the user flow through process' do
+  it 'goes to root path' do
+    visit root_path
+    expect(page).to have_link "Toyota"
+  end
+
   it 'goes from root to manufacturers#show' do
     visit root_path
-    click 'Toyota'
+    click_link("Toyota", exact: true )
     expect(page).to have_content "Select your Toyota model"
   end
 
-  it 'selects a Toyota model' do
-    visit manufacturer_path(manufacurer)
-    click '4runnner'
-    expect(page).to have_content "2015 4Runner Accessories"
-  end
+  # it 'selects a Toyota model' do
+  #   visit manufacturer_path(manufacturer)
+  #   click_on '4runnner'
+  #   expect(page).to have_content "2015 4Runner Accessories"
+  # end
 end
