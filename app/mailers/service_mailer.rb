ec9781c83ef460ea96b20e73817ab460bca48e38
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-04-28 15:32:12 -0700

    uncomment emails

--- app/mailers/service_mailer.rb
+++ app/mailers/service_mailer.rb
@@ -8,15 +8,11 @@ class ServiceMailer < ApplicationMailer
     @associate = @checkout.associate
     @order_items = order_items
 
-    # Delete line when ready for production
-    mail(:to => 'joshuaatteberry@gmail.com', :subject => "Customer Accessories Request")
-
-    # Uncomment the code below when ready for production
-    # mail(:to => @associate.email, :subject => "Customer Accessories Request",
-    #   :cc => ["stevey@toyotacorvallis.com", "chrish@toyotacorvallis.com",
-    #     "scottg@toyotacorvallis.com", "bridgetteh@toyotacorvallis.com",
-    #     "garyc@toyotacorvallis.com", "brianm@toyotacorvallis.com",
-    #     "charlesp@toyotacorvallis.com", "joshuaatteberry@gmail.com"])
+    mail(:to => @associate.email, :subject => "Customer Accessories Request",
+      :cc => ["stevey@toyotacorvallis.com", "chrish@toyotacorvallis.com",
+        "scottg@toyotacorvallis.com", "bridgetteh@toyotacorvallis.com",
+        "garyc@toyotacorvallis.com", "brianm@toyotacorvallis.com",
+        "charlesp@toyotacorvallis.com", "joshuaatteberry@gmail.com"])
   end
 end
 
