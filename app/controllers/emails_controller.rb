78996cd74c5577373c8741236c62980ae2c70d39
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-05-13 22:47:13 -0700

    edit emails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,4 +3,20 @@ class EmailsController < ApplicationController
   def edit
     @email = Email.find(1)
   end
+
+  def update
+    @email = Email.find(1)
+    if @email.update(email_params)
+      flash[:notice] = "Your emails have been updated successfully"
+      redirect_to console_index_path
+    else
+      flash[:error] = "There was an error with your email edit"
+      render action: :edit
+    end
+  end
+
+private
+  def email_params
+    params.require(:email).permit(:address)
+  end
 end
